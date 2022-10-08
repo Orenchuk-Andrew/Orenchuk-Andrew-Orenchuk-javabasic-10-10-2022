822b101e54b4a38291fd35b06ad41431612eac85
Orenchuk-Andrew <orenchyk.andrew@gmail.com>
2022-10-08 10:49:59 +0300

    New coordinate

---
+++ Hillel/src/Coordinate.java
@@ -0,0 +1,4 @@
+public class Coordinate {
+    public double x;
+    public double y;
+}
--- Hillel/src/Main.java
+++ Hillel/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+     //   System.out.println("Hi");
+
+  Coordinate Church = new Coordinate();
+  Church.x=50.43464044842177;
+  Church.y=30.55725228516998;
+        System.out.println(Church.x);
+        System.out.println(Church.y);
     }
 }
